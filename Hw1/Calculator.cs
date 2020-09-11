fe62d95bd68965a0febb5d95a8d308470c1d1cec
Railina <railina30@yandex.ru>
2020-09-11 21:09:30 +0300

    first

---
+++ Hw1/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw1
+{
+    class Calculator
+    {
+        public static int GetNumber()
+        {
+            return int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        }
+
+        public static int Calculate(string @operator, int a, int b)
+        {
+            var result = @operator switch
+            {
+                "+" => a + b,
+                "-" => a - b,
+                "*" => a * b,
+                "/" => a / b,
+                _ => throw new NotSupportedException()
+            };
+            return result;
+        }
+    }
+}
