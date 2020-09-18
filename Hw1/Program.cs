ccad2db29edf28a2e7ad28d311c0d654cab14106
RaiRG <64270610+RaiRG@users.noreply.github.com>
2020-09-18 15:02:18 +0300

    Hw1 (#1)
    * first
    
    * Add tests

---
+++ Hw1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static Hw1.Calculator;
+
+namespace Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = GetNumber();
+            var @operator = Console.ReadLine();
+            var b = GetNumber();
+            var result = Calculate(@operator, a, b);
+            Console.WriteLine(result);
+
+        }
+    }
+}
