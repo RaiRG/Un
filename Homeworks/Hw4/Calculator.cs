29137312716c961c9a18434d157c23a0ff3ae3f0
Railina <railina30@yandex.ru>
2020-10-12 10:47:10 +0300

    Add a middleware type to the application's request pipeline.

---
+++ Homeworks/Hw4/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw4
+{
+    public class Calculator
+    {
+        public static int Calculate(string @operator, int a, int b)
+        {
+            var result = @operator switch
+            {
+                "+" => a + b,
+                "-" => a - b,
+                "*" => a * b,
+                "/" => b == 0 ? throw new DivideByZeroException() : a / b,
+                _ => throw new NotSupportedException()
+            };
+            return result;
+        }
+    }
+}
---
+++ Homeworks/Hw4/MiddlewareCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Hw4
+{
+    public class MiddlewareCalculator
+    {
+        //use calculator!!!
+    }
+}
