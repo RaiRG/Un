bcdbc6db4730ddf5c95d44b6e90e4a8a4b6222c6
Railina <railina30@yandex.ru>
2020-10-12 12:54:06 +0300

    Edit Calculator

--- Homeworks/Hw4/MiddlewareCalculator.cs
+++ Homeworks/Hw4/MiddlewareCalculator.cs
@@ -6,6 +6,19 @@ namespace Hw4
 {
     public class MiddlewareCalculator
     {
+        private readonly RequestDelegate _next;
+        // СДлеать Pattern!! 
+
+        public MiddlewareCalculator(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _next.Invoke(context);
+            
+        }
         //use calculator!!!
     }
 }
--- Homeworks/Hw4/Startup.cs
+++ Homeworks/Hw4/Startup.cs
@@ -31,24 +31,8 @@ namespace Hw4
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCalculator();
+            app.Run (async (context) => await context.Response.WriteAsync("Part Run"));
             
-            
-            // if (env.IsDevelopment())
-            // {
-            //     app.UseDeveloperExceptionPage();
-            // }
-            // else
-            // {
-            //     app.UseExceptionHandler("/Error");
-            //     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //     app.UseHsts();
-            // }
-
-            // app.UseHttpsRedirection();
-            // app.UseStaticFiles();
-            // app.UseRouting();
-            // app.UseAuthorization();
-            // app.UseEndpoints(endpoints => { endpoints.MapRazorPages(); });
         }
     }
 }
