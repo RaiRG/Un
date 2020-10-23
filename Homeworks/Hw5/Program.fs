271cb1288b7c27fce2a768d3c89fbdb112e6846a
Railina <railina30@yandex.ru>
2020-10-23 23:25:51 +0300

    Add checking of input

--- Homeworks/Hw5/Program.fs
+++ Homeworks/Hw5/Program.fs
@@ -47,20 +47,69 @@ module Calculator =
                 let url =   path + x + correctOperator + y                  
                 let! result = getAnswer url 
                 return result
-       }        
-            
+       }       
 
+type MaybeBuilder() =
+
+    member this.Bind(x, f) =
+        match x with
+        | None -> None
+        | Some a -> f a
+     
+     member this.Return(x) = Some x
+     
 module Main =
+    let maybe = MaybeBuilder()
     let print (result : 'a option) =
                match result with
                |None -> Console.WriteLine("None")
                |_ -> Console.WriteLine(result.Value)
-    
+        
+    let tryDouble (vall:string) =         
+         maybe {
+         let! newVal = 
+            try
+                Some(Convert.ToDouble(vall))
+            with
+                | :? System.FormatException -> None
+         
+         return newVal                
+         }
+                
+    let getAnswer (vall : float option)=        
+        match vall  with
+        | None -> false
+        | _ -> true
+        
+    let checkVal (x:string) =
+        let y = tryDouble x
+        getAnswer y
+        
+    let checkOper(x:string) =
+        match x with
+        |"+" -> true
+        |"/" -> true
+        |"*" -> true
+        |"-" -> true
+        | _ -> false
+        
+            
     [<EntryPoint>]
     let main _ =
+        Console.WriteLine("Введите выражение:")
         let x = Console.ReadLine()
-        let operator = Console.ReadLine()
-        let y = Console.ReadLine()      
-        let result = Async.RunSynchronously(Calculator.calculate operator x y)
-        print result
+        let checkX = checkVal x
+        match checkX with
+            |false -> Console.WriteLine("Неверный формат!")
+            |true -> let operator = Console.ReadLine()
+                     let checkOp = checkOper operator
+                     match checkOp with
+                        |false -> Console.WriteLine("Неверный формат!")
+                        |true -> let y = Console.ReadLine()
+                                 let checkY = checkVal x
+                                 match checkY with
+                                        |false -> Console.WriteLine("Неверный формат!")
+                                        |true -> let z = Convert.ToDouble(x)
+                                                 let result = Async.RunSynchronously(Calculator.calculate operator x y)
+                                                 print result
         0
