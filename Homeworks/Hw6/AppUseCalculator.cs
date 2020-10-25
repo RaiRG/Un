3bb9d43bd2bc99cd3ce72a1582a634449fd85a64
Railina <railina30@yandex.ru>
2020-10-25 21:39:20 +0300

    Add client and calculator. Incomlete

---
+++ Homeworks/Hw6/AppUseCalculator.cs
@@ -0,0 +1,15 @@
+﻿﻿using Microsoft.AspNetCore.Builder;
+
+namespace Hw6
+{
+    public static class AppUseCalculator
+    {
+        public static void UseCalculator(this IApplicationBuilder app)
+        {
+            //Add a middleware type to the application's request pipeline.
+            app.UseMiddleware<MiddlewareCalculator>();
+        }
+        
+        
+    }
+}
---
+++ Homeworks/Hw6/MiddlewareCalculator.cs
@@ -0,0 +1,35 @@
+﻿﻿﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Hw6
+{
+    public class MiddlewareCalculator
+    {
+        private readonly RequestDelegate _next;
+        
+        public MiddlewareCalculator(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var expression = context.Request.Query["expression"];
+            var result = Calculator.CalculateExpression(expression);
+            
+            if (result == "Error")
+            {
+                await context.Response.WriteAsync("Invalid data format!");
+                context.Response.StatusCode = 400;
+            }
+            else
+            {
+                context.Response.StatusCode = 200;
+                context.Response.Headers.Add("result", result);
+                await context.Response.WriteAsync(result);
+            }
+            await _next(context);
+        }
+    }
+}
