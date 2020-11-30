3b536d650209cdc5c939612ce69b27a752daebeb
Railina <railina30@yandex.ru>
2020-11-30 12:56:34 +0300

    My Visitor

---
+++ Homeworks/Hw10/MyVisitor.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace Hw10
+{
+    public class MyVisitor : ExpressionVisitor
+    {
+        public Expression Visit(dynamic node)
+        {
+            return VisitExpression(node);
+        }
+
+        private Expression VisitExpression(BinaryExpression expression)
+        {
+            Visit(expression.Right);
+            Visit(expression.Left);
+            return expression;
+        }
+        private Expression VisitExpression(ConstantExpression expression)
+        {
+            
+            //
+        }
+       
+        
+    }
+}
